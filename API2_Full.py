c4a5ea1bcf266940f00e9b971b54c1aa45b52fcf
BAIBASH1 <124410504+BAIBASH1@users.noreply.github.com>
2023-02-28 15:07:11 +0300

    Update API2_Full.py
    исправлены некоторые синтаксические ошибки, улучшен текст программы

--- API2_Full.py
+++ API2_Full.py
@@ -1,55 +1,60 @@
 import requests
-import json
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 import os
 
 
 def is_bitlink(token, url):
-    o = urlparse(url)
-    headers = {
+    divided_url = urlparse(url)
+    header = {
         'Authorization': token
     }
-    url_full = 'https://api-ssl.bitly.com/v4/bitlinks/' + o.netloc + o.path
-    response = requests.get(url_full, headers=headers)
+    full_url = f'https://api-ssl.bitly.com/v4/bitlinks/ {divided_url.netloc} {divided_url.path}'
+    response = requests.get(full_url, headers=header)
     return response.ok
 
 
 def shorten_link(token, url):
-    headers = {
+    header = {
         'Authorization': token
     }
-    data = {
+    long_url = {
         "long_url": url
     }
-    response = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=headers, data=json.dumps(data))
+    response = requests.post("https://api-ssl.bitly.com/v4/shorten", headers=header, json=long_url)
     response.raise_for_status()
     bitlink = response.json()["link"]
     return bitlink
 
 
 def count_clicks(token, url):
-    o = urlparse(url)
-    url_full = 'https://api-ssl.bitly.com/v4/bitlinks/' + o.netloc + o.path + '/clicks'
-    headers = {
+    divided_url = urlparse(url)
+    full_url = f'https://api-ssl.bitly.com/v4/bitlinks/ {divided_url.netloc} {divided_url.path} {"/clicks"}'
+    print(full_url)
+    print(divided_url.netloc)
+    print(divided_url.path)
+    header = {
         'Authorization': token
     }
     params = {
         'unit': 'day',
         'units': '-1'
     }
-    response = requests.get(url_full, headers=headers, params=params)
+    response = requests.get(full_url, headers=header, params=params)
     response.raise_for_status()
     cliks = response.json()['link_clicks'][0]['clicks']
     return cliks
 
 
-if __name__ == '__main__':
-    load_dotenv('o.env')
-    token = os.environ['TOKEN']
-    url = input('Введите ссылку: ')
-    if is_bitlink(token, url):
-        print('Количество кликов:', count_clicks(token, url))
-    else:
-        print('Ваш битлинк:', shorten_link(token, url))
+def main():
+    if __name__ == '__main__':
+        load_dotenv('ID.env')
+        token = os.environ['BITLINK_TOKEN']
+        url = input('Введите ссылку: ')
+        if is_bitlink(token, url):
+            print('Количество кликов:', count_clicks(token, url))
+        else:
+            print('Ваш битлинк:', shorten_link(token, url))
 
+
+main()
